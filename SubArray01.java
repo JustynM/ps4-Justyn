7dc4ab4394d0a73b273b2f6f80206e8d1d97991b
JustynM <jm14mcha@siena.edu>
2018-03-12 18:23:50 -0400

    kjg

---
+++ SubArray01.java
@@ -0,0 +1,100 @@
+
+/**
+ * Write a description of class SubArray01 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SubArray01
+{
+    public int main(int[] A){
+        int countSub = 0;
+        for(int i = 0; i < A.length; i++){
+            if(A[i]==0){
+                for(int j = i; j < A.length; j++){
+                    if(A[j]==1){
+                        countSub++;
+                    }
+                }
+            }
+        }
+        return countSub;
+    }
+
+    public int RecursiveLinear(int[] A){
+        int countSub = 0;
+        if (A.length == 0){
+            return 0;
+        }
+        else if(A[0]!=0){
+            int[] B = new int[A.length-1];
+            for(int i = 1; i < A.length; i++){
+                B[i-1] = A[i];
+            }
+            return RecursiveLinear(B);
+        }
+        else{
+            for(int i = 0; i < A.length; i++){
+                if(A[i] == 1){
+                    countSub++;
+                }
+            }
+            int[] B = new int[A.length-1];
+            for(int i = 1; i < A.length; i++){
+                B[i-1] = A[i];
+            }
+            return RecursiveLinear(B) + countSub;
+        }
+    }
+
+    public int MaxProfit(int[] A, int sday, int eday){
+        int maxValue = 0;
+        if(sday == eday){
+            return A[sday];
+        }
+        else 
+        {
+            int[] values = new int[A.length];
+            for (int i = sday; i <= eday; i++){
+                if(i==sday){
+                    values[i] = A[i];
+                }
+                else{
+                    values[i] = A[i] + values[i-1];
+                }
+                if (values[i] > maxValue){
+                    maxValue = values[i];
+                }
+            }
+            if (MaxProfit(A,sday + 1, eday) < maxValue){
+                return maxValue;
+            }
+            else{
+                return MaxProfit(A,sday + 1, eday);
+            }
+        }
+    }
+    
+    public void MaxHalf(int[] A, int sday, int eday){
+        int[] B = new int[A.length/2];
+        int[] C = new int[A.length/2];
+        if(A.length > 1){
+            for(int i = 0; i < Math.floor(A.length/2); i++){
+                B[i] = A[i];
+            }
+            for(int i = (int) Math.floor(A.length/2); i < A.length; i++){
+                C[i] = A[i];
+            }
+            MaxHalf(B,sday,(int)Math.floor(eday/2));
+            MaxHalf(C,(int)Math.floor(eday/2),eday);
+            int total = MaxProfit(A,sday,eday);
+            int totalB = MaxProfit(B,sday,(int)Math.floor(eday/2));
+            int totalC = MaxProfit(C,(int)Math.floor(eday/2),eday);
+            if(totalB > total){
+                
+            }
+        }
+        
+        
+    }
+}
